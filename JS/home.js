edc09108314367e33f614b00924d6cbd9e838f7f
Sayamgada <sayamgada2005@gmail.com>
2024-12-17 15:49:01 +0530

    forth

--- JS/home.js
+++ JS/home.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const marqueeContainer = document.querySelector('.marquee-container'); 
     const marqueeTexts = document.querySelectorAll('.marquee-text'); 
 
+
     marqueeContainer.addEventListener('click', () => {
         marqueeTexts.forEach(text => {
             text.style.animationPlayState = 'paused';
