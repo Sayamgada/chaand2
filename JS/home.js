da1a38f7b8e6bd61017888ee5f2598440b98a12f
Sayamgada <sayamgada2005@gmail.com>
2024-12-17 14:30:43 +0530

    sec

--- JS/home.js
+++ JS/home.js
@@ -1,15 +1,20 @@
- // Pause animation on hover for better interactivity
- const marqueeContainer = document.querySelector('.marquee-container');
- const marqueeTexts = document.querySelectorAll('.marquee-text');
+// marquee
+document.addEventListener('DOMContentLoaded', () => {
+    const marqueeContainer = document.querySelector('.marquee-container'); 
+    const marqueeTexts = document.querySelectorAll('.marquee-text'); 
 
- marqueeContainer.addEventListener('mouseenter', () => {
-     marqueeTexts.forEach(text => {
-         text.style.animationPlayState = 'paused';
-     });
- });
+    marqueeContainer.addEventListener('click', () => {
+        marqueeTexts.forEach(text => {
+            text.style.animationPlayState = 'paused';
+        });
+    });
 
- marqueeContainer.addEventListener('mouseleave', () => {
-     marqueeTexts.forEach(text => {
-         text.style.animationPlayState = 'running';
-     });
- });
+    marqueeContainer.addEventListener('mouseleave', () => {
+        marqueeTexts.forEach(text => {
+            text.style.animationPlayState = 'running';
+        });
+    });
+});
+
+
+
