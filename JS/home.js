72db40e821295aed63d2a0e0f6e458788d85c2da
Sayamgada <sayamgada2005@gmail.com>
2024-12-29 13:34:15 +0530

    twentyone

--- JS/home.js
+++ JS/home.js
@@ -1,3 +1,5 @@
+
+// marquee
 document.addEventListener('DOMContentLoaded', () => {
     const marqueeContainer = document.querySelector('.marquee-container'); 
     const marqueeTexts = document.querySelectorAll('.marquee-text'); 
@@ -9,33 +11,21 @@ document.addEventListener('DOMContentLoaded', () => {
     marqueeContainer.addEventListener('mouseleave', () => {
         marqueeTexts.forEach(text => text.style.animationPlayState = 'running');
     });
-
-    // Dynamically add marquee text
-    const newText = document.createElement('div');
-    newText.className = 'marquee-text';
-    newText.textContent = '| New Arrivals |';
-    marqueeContainer.appendChild(newText);
 });
 
-
+// carousel
+window.onload = function() { 
+    sliderContainer = document.getElementById('slider-container'); 
+};
 const sliderItems = document.querySelectorAll('.slider-item');
-const indicators = document.querySelector('.carousel-indicator');
-
-sliderItems.forEach((item, index) => {
-    const dot = document.createElement('span');
-    dot.dataset.index = index;
-    indicators.appendChild(dot);
-});
-
-indicators.children[0].classList.add('active');
-
+const totalItems = sliderItems.length;
 let currentIndex = 0;
-function updateActiveDot() {
-    indicators.querySelectorAll('span').forEach(dot => dot.classList.remove('active'));
-    indicators.children[currentIndex].classList.add('active');
-}
 
-setInterval(() => {
-    currentIndex = (currentIndex + 1) % sliderItems.length;
-    updateActiveDot();
-}, 3000);
+function auto_slide() {
+    if (sliderContainer) 
+    {
+        currentIndex = (currentIndex + 1) % totalItems;
+        sliderContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+    } 
+}
+setInterval(auto_slide, 3000);
